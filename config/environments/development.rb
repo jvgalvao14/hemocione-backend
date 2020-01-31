ef02e1e4f052a78cb4a65193e4739d24a27225aa
Thiago Guimaraes <thiagoguimaraes.dev@gmail.com>
2020-01-31 18:05:49 -0300

    Fix config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "api.local.hemocione.net"
+  config.hosts << 'api.local.hemocione.net'
 end
