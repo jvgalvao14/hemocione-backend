c233922efa2f470623e40df5aeccdd15271cd26e
Thiago Guimaraes <thiagoguimaraes.dev@gmail.com>
2020-02-17 18:22:55 -0300

    WIP migrations for subs and events- CHECK for MORE DATA TO BE INSIDE DB

---
+++ db/migrate/20200217210934_add_subscriptions_and_events.rb
@@ -0,0 +1,21 @@
+class AddSubscriptionsAndEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table(:institutions) do |t|
+      t.string 'name'
+      t.timestamps
+    end
+
+    create_table(:events) do |t|
+      t.string 'name'
+      t.string 'type'
+      t.belongs_to :institution
+      t.timestamps
+    end
+
+    create_table(:subscriptions) do |t|
+      t.belongs_to :user
+      t.belongs_to :event
+      t.timestamps
+    end
+  end
+end
