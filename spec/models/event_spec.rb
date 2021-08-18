31902f619bf0e5bda71329390ea179a8e1831f9b
Pedro Teixeira <pedroteixeir@poli.ufrj.br>
2021-08-17 22:15:05 -0300

    Added event validations

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+    describe 'associations' do
+        it { is_expected.to have_many(:subscriptions) }
+        it { is_expected.to have_many(:subscribers) }
+        it { is_expected.to belong_to(:institution) }
+        it { is_expected.to have_one(:event_address) }
+    end
+
+    describe 'validations' do
+        it { is_expected.to validate_presence_of(:event_type) }
+        it { is_expected.to validate_presence_of(:name) }
+        it { is_expected.to validate_presence_of(:description) }
+        it { is_expected.to validate_presence_of(:start_at) }
+        it { is_expected.to validate_presence_of(:end_at) }
+        it { is_expected.to validate_inclusion_of(:event_type).in_array(%w[standard dia_hemocione enterprise remoto]) }
+    end
+end
