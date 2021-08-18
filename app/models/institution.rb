49bc17d1d18ce0506518199e0cea3e72818216b5
Pedro Teixeira <pedroteixeir@poli.ufrj.br>
2021-08-17 21:33:22 -0300

    Modifying models of intitution and institution_adress

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 class Institution < ApplicationRecord
-    has_many :institution_addresses
+    has_many :institution_addresses, foreign_key: :entity_id
 
     before_validation :set_cnpj_correctly
 
-    validates :legal_name, presence: true
-    validates :cnpj, presence: true
+    validates :legal_name, :cnpj, presence: true
+    validates :legal_name, :cnpj, uniqueness: true 
     validate :cnpj_must_be_valid
 
     def cnpj_must_be_valid
-        errors.add(:cnpj, "is not valid") unless CNPJ.valid?(cnpj)
+        errors.add(:cnpj, "cnpj inválido") unless CNPJ.valid?(self.cnpj)
     end
 
+    private
+
     def set_cnpj_correctly
-        self.cnpj = self.cnpj.gsub(/[^\d]/,"")
+        self.cnpj = self.cnpj&.gsub(/[^\d]/,"")
     end
 end
--- app/models/institution_address.rb
+++ app/models/institution_address.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class InstitutionAddress < EntityAddress
-    belongs_to :institution
+    belongs_to :institution, foreign_key: :entity_id
 end
