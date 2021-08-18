12693de928bf2066a9dfbd838577865760672699
Pedro Teixeira <pedroteixeir@poli.ufrj.br>
2021-08-17 22:14:50 -0300

    Changed event model in order to add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  has_many :subscriptions
-  has_many :subscribers, through: :subscriptions, foreign_key: "user_id", class_name: "User"
-  belongs_to :institution
-  has_one :event_address
+    has_many :subscriptions
+    has_many :subscribers, through: :subscriptions, foreign_key: "user_id", source: "user"
+    belongs_to :institution
+    has_one :event_address, foreign_key: :entity_id
+
+    validates :name, :description, :event_type, :start_at, :end_at, presence: true
+    validates :event_type, inclusion: { in: %w[standard dia_hemocione enterprise remoto] }
+    validate :end_at_after_start_at
+
+    private
+
+    def end_at_after_start_at
+        errors.add(:end_at, "data de fim deve ser posterior a data de inicio") unless self.end_at.to_i > self.start_at.to_i
+    end
 end
