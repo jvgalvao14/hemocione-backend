9c7e8900b2375e6366452cf7965ded45fd2adf03
Pedro Teixeira <pedroteixeir@poli.ufrj.br>
2021-08-17 21:33:49 -0300

    Removed hard-codded test for user email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:document_id).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_inclusion_of(:document_type).in_array(%w[CPF CNPJ]) }
-    it { is_expected.not_to allow_value('email').for(:email) }
-    it { is_expected.to allow_value('email@inbucket.com').for(:email) }
+    it { is_expected.not_to allow_value(Faker::Games::Pokemon.name).for(:email) }
+    it { is_expected.to allow_value(Faker::Internet.unique.email).for(:email) }
   end
 end
